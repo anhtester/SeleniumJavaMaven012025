ea07ea19d4ce9572c225b5a8cd156103a52fc7fb
An Vo <thaian.it15@gmail.com>
2025-04-02 19:35:22 +0700

    Add bài 5 locators

---
+++ src/test/java/com/anhtester/Bai5_Locators/DemoHTMLAttribute.java
@@ -0,0 +1,7 @@
+package com.anhtester.Bai5_Locators;
+
+public class DemoHTMLAttribute {
+    public static void main(String[] args) {
+        System.out.println("Locator HTML Attribute.");
+    }
+}
